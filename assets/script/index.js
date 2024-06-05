780e0513cb8421e384b565acd98fd5b5ea965582
gki-webik <gki-webik@yandex.ru>
2024-06-05 18:49:13 +0600

    add project

---
+++ assets/script/index.js
@@ -0,0 +1,88 @@
+const close_btn = document.querySelector(".close_btn");
+const open_btn = document.querySelector(".open_btn");
+const header__menu = document.querySelector(".header__menu");
+const menu_mask = document.querySelector(".menu_mask");
+const button_basket = document.querySelectorAll(".button_basket");
+const header__menu_right_basket = document.querySelector(".header__menu_right_basket");
+const basket_modal = document.querySelector(".basket_modal");
+const basket_modal__cards__close_btn = document.querySelector(".basket_modal__cards__close_btn");
+const basket_item_box = document.querySelector(".basket_item_box");
+
+close_btn.addEventListener("click", () => {
+    header__menu.classList.toggle("is-active");
+    menu_mask.classList.toggle("is-active");
+});
+open_btn.addEventListener("click", () => {
+    header__menu.classList.toggle("is-active");
+    menu_mask.classList.toggle("is-active");
+});
+
+button_basket.forEach((e) => {
+    e.addEventListener("click", () => {
+        const idProduct = Number(e.parentNode.parentNode.parentNode.parentNode.dataset.id);
+        if (localStorage.getItem("wk_basket")) {
+            let wk_basket = JSON.parse(localStorage.getItem("wk_basket"));
+            wk_basket.forEach((i) => {
+                if (idProduct !== i.id) {
+                    const wk_basket_newObj = {
+                        id: idProduct,
+                        count: 1,
+                        title: e.parentNode.parentNode.parentNode.parentNode.children[1].children[0].textContent,
+                        price: e.parentNode.parentNode.parentNode.parentNode.children[2].children[1].textContent,
+                        img: e.parentNode.parentNode.parentNode.parentNode.children[0].children[0].src,
+                    };
+                    wk_basket.push(wk_basket_newObj);
+                    localStorage.setItem("wk_basket", JSON.stringify(wk_basket));
+                } else {
+                    console.log("Уже есть");
+                }
+            });
+        } else {
+            let wk_basket = [];
+            const wk_basket_newObj = {
+                id: idProduct,
+                count: 1,
+                title: e.parentNode.parentNode.parentNode.parentNode.children[1].children[0].textContent,
+                price: e.parentNode.parentNode.parentNode.parentNode.children[2].children[1].textContent,
+                img: e.parentNode.parentNode.parentNode.parentNode.children[0].children[0].src,
+            };
+            wk_basket.push(wk_basket_newObj);
+            localStorage.setItem("wk_basket", JSON.stringify(wk_basket));
+        }
+    });
+});
+function wk_basket_update() {
+    if (localStorage.getItem("wk_basket")) {
+        const wk_basket = JSON.parse(localStorage.getItem("wk_basket"));
+        basket_item_box.innerHTML = ``;
+        wk_basket.forEach((i) => {
+            basket_item_box.innerHTML += `
+        <div class="basket_modal__item">
+            <div class="basket_modal__item_img"><img src="${i.img}" alt=""></div>
+            <div class="basket_modal__item_content">
+                <div class="basket_modal__item_title">${i.title}</div>
+                <div class="basket_modal__item_btn">
+                    <div class="basket_modal__item_count"><span>+</span><span>${i.count}</span><span>-</span></div>
+                    <div class="basket_modal__item_price">${i.price}</div>
+                    <div class="basket_modal__item_delete">×</div>
+                </div>
+            </div>
+        </div>
+        `;
+        });
+    } else {
+        basket_item_box.innerHTML = `
+    <div class="basket_modal__item">
+      Вы еще не добавили товаров в корзину
+    </div>
+    `;
+    }
+}
+header__menu_right_basket.addEventListener("click", () => {
+    basket_modal.classList.add("is-active");
+    wk_basket_update();
+});
+basket_modal__cards__close_btn.addEventListener("click", () => {
+    basket_modal.classList.remove("is-active");
+    wk_basket_update();
+});
