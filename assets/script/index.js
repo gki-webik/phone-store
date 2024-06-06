7f5c4e63a5fcb4bc0a9a7d6e0770809f42be550c
gki-webik <gki-webik@yandex.ru>
2024-06-06 23:17:11 +0600

    del bug in filter and basket

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,15 +2,14 @@ const close_btn = document.querySelector(".close_btn");
 const open_btn = document.querySelector(".open_btn");
 const header__menu = document.querySelector(".header__menu");
 const menu_mask = document.querySelector(".menu_mask");
-const button_basket = document.querySelectorAll(".button_basket");
 const header__menu_right_basket = document.querySelectorAll(".header__menu_right_basket");
 const basket_modal = document.querySelector(".basket_modal");
 const basket_modal__cards__close_btn = document.querySelectorAll(".basket_modal__cards__close_btn");
 const basket_item_box = document.querySelector(".basket_item_box");
-const basket_items = document.querySelectorAll(".block_3__main_cards_item");
 const basket_plusCount = document.querySelectorAll(".basket_plusCount");
 const block_3__main_cards = document.querySelector(".block_3__main_cards");
 const checkbox_filter = document.querySelectorAll('.checkbox_filter');
+const checkbox_filter_model = document.querySelectorAll('.checkbox_filter_model');
 
 let wk_basket = JSON.parse(localStorage.getItem("wk_basket")) || [];
 
@@ -25,10 +24,13 @@ open_btn.addEventListener("click", () => {
 });
 
 /* Всем кнопкам на товары, что в корзине, кнопка Удалить из корзины */
-basket_items.forEach((e) => {
-    if (wk_basket.some(item => Number(e.dataset.id) === item.id)) {
-        e.querySelector(".button_basket").textContent = "Удалить";
-    }
+document.addEventListener("DOMContentLoaded", () => {
+    const basket_items = document.querySelectorAll(".block_3__main_cards_item");
+    basket_items.forEach((e) => {
+        if (wk_basket.some(item => Number(e.dataset.id) === item.id)) {
+            e.querySelector(".button_basket").textContent = "Удалить";
+        }
+    });
 });
 
 /* Поиск индекса элемента */
@@ -57,29 +59,35 @@ function updCountBasket(props) {
 updCountBasket();
 
 /* Добавить/Удалить товар  */
-function buttonBasket(props) {
-    const bsItemParentNode = document.querySelector(".block_3__main_cards_item[data-id='" + props + "']");
-    const idProduct = props;
-    bsItemParentNode.querySelector(".button_basket").textContent = "Удалить";
+document.addEventListener("DOMContentLoaded", () => {
+    const button_basket = document.querySelectorAll(".button_basket");
+    button_basket.forEach((e) => {
+        e.addEventListener("click", () => {
+            const bsItemParentNode = e.parentNode.parentNode.parentNode.parentNode;
+            const idProduct = Number(bsItemParentNode.dataset.id);
+            bsItemParentNode.querySelector(".button_basket").textContent = "Удалить";
 
-    if (wk_basket.some(item => idProduct === item.id)) {
-        updCountBasket("minus");
-        bsItemParentNode.querySelector(".button_basket").textContent = "В корзину";
-        wk_basket.splice(functiontofindIndexByKeyValue(wk_basket, "id", idProduct), 1);
-        localStorage.setItem("wk_basket", JSON.stringify(wk_basket));
-        return;
-    }
-    const wk_basket_newObj = {
-        id: idProduct,
-        count: 1,
-        title: bsItemParentNode.querySelector(".block_3__main_cards_item__center_title").textContent,
-        price: bsItemParentNode.querySelector(".block_3__main_cards_item__bottom_2__price").textContent,
-        img: bsItemParentNode.querySelector(".block_3__main_cards_item__img > img").src,
-    };
-    wk_basket.push(wk_basket_newObj);
-    localStorage.setItem("wk_basket", JSON.stringify(wk_basket));
-    updCountBasket();
-}
+            if (wk_basket.some(item => idProduct === item.id)) {
+                updCountBasket("minus");
+                bsItemParentNode.querySelector(".button_basket").textContent = "В корзину";
+                wk_basket.splice(functiontofindIndexByKeyValue(wk_basket, "id", idProduct), 1);
+                localStorage.setItem("wk_basket", JSON.stringify(wk_basket));
+                return;
+            }
+            const wk_basket_newObj = {
+                id: idProduct,
+                count: 1,
+                title: bsItemParentNode.querySelector(".block_3__main_cards_item__center_title").textContent,
+                price: bsItemParentNode.querySelector(".block_3__main_cards_item__bottom_2__price").textContent,
+                img: bsItemParentNode.querySelector(".block_3__main_cards_item__img > img").src,
+            };
+            wk_basket.push(wk_basket_newObj);
+            localStorage.setItem("wk_basket", JSON.stringify(wk_basket));
+            updCountBasket();
+        });
+    });
+});
+
 /* Отображение товаров в корзине */
 function wk_basket_update() {
     if (localStorage.getItem("wk_basket")) {
@@ -91,35 +99,10 @@ function wk_basket_update() {
             wk_basket.forEach((i) => {
                 resultCountAll += i.count;
                 resultPrice += Number(i.price.replaceAll("$", "")) * i.count;
-                basket_item_box.innerHTML += `
-            <div class="basket_modal__item" data-id="${i.id}">
-                <div class="basket_modal__item_img"><img src="${i.img}" alt=""></div>
-                <div class="basket_modal__item_content">
-                    <div class="basket_modal__item_title">${i.title}</div>
-                    <div class="basket_modal__item_btn">
-                        <div class="basket_modal__item_count"><span onclick="basketCount(${i.id}, 'minus')">-</span><span>${i.count}</span><span onclick="basketCount(${i.id}, 'plus')">+</span></div>
-                        <div class="basket_modal__item_price">${Number(i.price.replaceAll("$", "")) * i.count}$</div>
-                        <div class="basket_modal__item_delete" onclick="basketCount(${i.id}, 'delete')">×</div>
-                    </div>
-                </div>
-            </div>
-            `;
+                basket_item_box.innerHTML += `<div class="basket_modal__item" data-id="${i.id}"><div class="basket_modal__item_img"><img src="${i.img}" alt=""></div><div class="basket_modal__item_content"><div class="basket_modal__item_title">${i.title}</div><div class="basket_modal__item_btn"><div class="basket_modal__item_count"><span onclick="basketCount(${i.id}, 'minus')">-</span><span>${i.count}</span><span onclick="basketCount(${i.id}, 'plus')">+</span></div><div class="basket_modal__item_price">${Number(i.price.replaceAll("$", "")) * i.count}$</div><div class="basket_modal__item_delete" onclick="basketCount(${i.id}, 'delete')">×</div></div></div></div>`;
             });
             resultCount += wk_basket.length
-            basket_item_box.innerHTML += `
-            <div class="basket_modal__item result">
-                <div>
-                <b>Товаров</b>: ${resultCount}
-                </div>
-                <div>
-                <b>Товаров (вкл. шт.)</b>: ${resultCountAll}
-                </div>
-                <div>
-                <b>Итоговая цена</b>: ${resultPrice}$
-                </div>
-                <button>Оформить</button>
-            </div>
-            `;
+            basket_item_box.innerHTML += `<div class="basket_modal__item result"><div><b>Товаров</b>: ${resultCount}</div><div><b>Товаров (вкл. шт.)</b>: ${resultCountAll}</div><div><b>Итоговая цена</b>: ${resultPrice}$</div><button>Оформить</button></div>`;
             return;
         }
     }
@@ -190,42 +173,19 @@ let arrProduct = [
 function outputProducts(props) {
     block_3__main_cards.innerHTML = "";
     props.forEach((item) => {
-        block_3__main_cards.innerHTML += `
-            <div class="block_3__main_cards_item" data-id="${item.id}" data-category="${item.category}">
-                            <div class="block_3__main_cards_item__img">
-                                <img src="${item.img}" alt="">
-                            </div>
-                            <div class="block_3__main_cards_item__center">
-                                <div class="block_3__main_cards_item__center_title">${item.title}</div>
-                                <div class="block_3__main_cards_item__center_color">
-                                    <span class="block_3__main_cards_item__center_color_1"></span>
-                                    <span class="block_3__main_cards_item__center_color_2"></span>
-                                    <span class="block_3__main_cards_item__center_color_3"></span>
-                                </div>
-                            </div>
-                            <div class="block_3__main_cards_item__bottom">
-                                <div class="block_3__main_cards_item__bottom_1">
-                                    <div class="block_3__main_cards_item__bottom_1__button">
-                                        <button class="button_basket" onclick="buttonBasket(${item.id})">В корзину</button>
-                                    </div>
-                                    <div class="block_3__main_cards_item__bottom_1__other">Подробнее</div>
-                                </div>
-                                <div class="block_3__main_cards_item__bottom_2__price">${item.price}$</div>
-                            </div>
-                        </div>
-            `;
+        block_3__main_cards.innerHTML += `<div class="block_3__main_cards_item" data-id="${item.id}" data-category="${item.category}"><div class="block_3__main_cards_item__img"><img src="${item.img}" alt=""></div><div class="block_3__main_cards_item__center"><div class="block_3__main_cards_item__center_title">${item.title}</div><div class="block_3__main_cards_item__center_color"><span class="block_3__main_cards_item__center_color_1"></span><span class="block_3__main_cards_item__center_color_2"></span><span class="block_3__main_cards_item__center_color_3"></span></div></div><div class="block_3__main_cards_item__bottom"><div class="block_3__main_cards_item__bottom_1"><div class="block_3__main_cards_item__bottom_1__button"><button class="button_basket">В корзину</button></div><div class="block_3__main_cards_item__bottom_1__other">Подробнее</div></div><div class="block_3__main_cards_item__bottom_2__price">${item.price}$</div></div></div>`;
     });
 
 }
 outputProducts(arrProduct);
 
 /* Фильтры */
-let newArray = arrProduct.slice();
-checkbox_filter.forEach(c => {
+var newArrayProduct = arrProduct.slice();
+checkbox_filter_model.forEach(c => {
     c.addEventListener("change", function () {
-        newArray = arrProduct.filter((item) => {
+        newArrayProduct = arrProduct.filter((item) => {
             return document.getElementById(item.category).checked;
         });
-        outputProducts(newArray);
+        outputProducts(newArrayProduct);
     });
 });
